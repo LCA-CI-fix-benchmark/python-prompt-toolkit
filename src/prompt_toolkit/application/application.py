5e88ba532bb6b35a42be45be1720a2dc2abb1405
ubuntu <ubuntu@ip-10-0-1-33>
2025-01-15 16:40:56 +0000

    waleko__claude-3.5-sonnet_d261__id_165

--- src/prompt_toolkit/application/application.py
+++ src/prompt_toolkit/application/application.py
@@ -233,8 +233,7 @@ class Application(Generic[_AppResult]):
         self.style = style
         self.style_transformation = style_transformation
 
-        # Key bindings.
-        self.key_bindings = key_bindings
+        # Key bindings. self.key_bindings = key_bindings
         self._default_bindings = load_key_bindings()
         self._page_navigation_bindings = load_page_navigation_bindings()
 
